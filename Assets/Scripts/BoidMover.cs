46122d58c7acbf0dde401d423324ded5066ca26b
hyojoon-kim <112752398+hyojoon328@users.noreply.github.com>
2024-12-02 14:44:18 -0800

    made some boid? clustering

---
+++ Assets/Scripts/BoidMover.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidMover : MonoBehaviour
+{
+
+    public UnitGenerator boidController;
+
+    private float speed;// = 0.03f;
+
+    List<BoidMover> neighbor = new List<BoidMover>();
+    [SerializeField] LayerMask boidUnitLayer;
+
+    Vector3 targetVec;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void InitializeUnit(UnitGenerator core, float _speed)
+    {
+        boidController = core;
+        speed = _speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindNeighbor();
+
+        Vector3 cohesionVec = GetCohesion() * boidController.cohesionWeight;
+        Vector3 alignmentVec = GetAlignment() * boidController.alignmentWeight;
+        Vector3 seperationVec = GetSeparation() * boidController.separationWeight;
+        // Steer and Move
+
+        targetVec = cohesionVec + alignmentVec + seperationVec;
+        targetVec = Vector3.Lerp(this.transform.forward, targetVec, Time.deltaTime);
+        targetVec = targetVec.normalized;
+        this.transform.rotation = Quaternion.LookRotation(targetVec);
+        //     this.transform.position += targetVec * (speed + additionalSpeed) * Time.deltaTime;
+
+
+        transform.position += transform.forward * speed;
+    }
+
+
+    private void FindNeighbor()
+    {
+        neighbor.Clear();
+
+        Collider[] colls = Physics.OverlapSphere(transform.position, 20f, boidUnitLayer);
+        for (int i = 0; i < colls.Length; i++)
+        {
+            neighbor.Add(colls[i].GetComponent<BoidMover>());
+        }
+
+
+
+    }
+
+    public Vector3 GetCohesion()
+    {
+        Vector3 cohesionVec = Vector3.zero;
+        if (neighbor.Count > 0)
+        {
+            for (int i = 0; i < neighbor.Count; i++)
+            {
+                cohesionVec += neighbor[i].transform.position;
+            }
+
+        }
+        else
+        {
+            return cohesionVec;
+        }
+
+        // 중심 위치로의 벡터 찾기
+        cohesionVec /= neighbor.Count;
+        cohesionVec -= transform.position;
+        cohesionVec.Normalize();
+        return cohesionVec;
+    }
+
+
+    public Vector3 GetAlignment()
+    {
+        Vector3 alignmentVec = transform.forward;
+        if (neighbor.Count > 0)
+        {
+            for (int i = 0; i < neighbor.Count; i++)
+            {
+                alignmentVec += neighbor[i].transform.forward;
+            }
+
+
+        }
+        else
+        {
+
+            return alignmentVec;
+        }
+
+        alignmentVec /= neighbor.Count;
+
+        return alignmentVec;
+
+    }
+
+    public Vector3 GetSeparation()
+    {
+        Vector3 separationVec = Vector3.zero;
+        if (neighbor.Count > 0)
+        {
+            for (int i = 0; i < neighbor.Count; i++)
+            {
+                separationVec += (transform.position - neighbor[i].transform.position);
+            }
+        }
+        else
+        {
+            return separationVec;
+        }
+        separationVec /= neighbor.Count;
+
+        return separationVec;
+    }
+
+}
